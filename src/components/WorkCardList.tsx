7f110730ffcd97ff91f2bce49dc587d3aa13cd7b
KoshiOsaki <osaki36dg@gmail.com>
2022-09-23 06:39:49 +0900

    worksの整理

--- src/components/WorkCardList.tsx
+++ src/components/WorkCardList.tsx
@@ -5,86 +5,10 @@ export const WorkCardList = () => {
   return (
     <>
       <WorkCard
-        title="株式会社FUTUREWOODS サービスページ"
-        img="/futuresearch.png"
-        link="https://www.future-search.jp/"
-        skill="Next.js, TypeScript, TailWindCSS"
-        role="フロント開発 (UIの実装)"
-        description=""
-      >
-        <Text>
-          株式会社FUTUREWOODSの長期インターンにて、学生3人で共同開発したサービスLP。初の業務だったためGithubの使い方や命名法、コンポーネント粒度など先輩のコードを見て真似した。レスポンシブ対応に注意して制作した。
-        </Text>
-      </WorkCard>
-      <WorkCard
-        title="株式会社FUTUREWOODS 企業ページ"
-        img="/futurewoods.png"
-        link="https://www.futurewoods.co.jp/"
-        skill="Next.js, TypeScript, TailWindCSS"
-        role="フロント開発 (UIの実装)"
-        description=""
-      >
-        <Text>
-          株式会社FUTUREWOODSの長期インターンにて、学生3人で共同開発した企業HP。Webデザイナーさんが用意した概形のデザインをうまくcssを用いてコード化した。動きがうるさすぎず、かつ飽きないといった程よいアニメーションとモダンなデザインが特徴。
-        </Text>
-      </WorkCard>
-      <WorkCard
-        title="株式会社FUTUREWOODS 業務用ダッシュボード"
-        img="/fs-admin.png"
-        link="fs-admin"
-        skill="Nest.js, Next.js, MySQL"
-        role="フロント開発、バックエンド開発(追加機能の実装、コードの整形)"
-        description="デモ動画"
-      >
-        <Text>
-          株式会社FUTUREWOODSの長期インターンにて、先輩から引き継ぎ、運用している業務用ダッシュボード。フロントをMUIを用いたデザインにリニューアルした後、バックエンドをexpressからNest.jsに移行・整形した。通知機能や新たなソート等の機能を適宜追加している。
-        </Text>
-      </WorkCard>
-      <WorkCard
-        title="Cell Festival"
-        img="/cellfestival.png"
-        link="https://cell-festival.vercel.app/"
-        skill="Next.js, TypeScript, React, Firebase"
-        role="フロント開発、記事の校閲、追加機能の考案"
-        description="制作中です。。"
-      >
-        <Text>
-          生物系の学習プラットフォーム制作を目指す学生団体Cell
-          Festivalのお手伝いにて、ホームページを制作。自分が生物専攻なのもあり、記事を実際に読んだり、追加機能を考えたりしながら開発している。記事の投稿、校閲をできる機能を作ったのが工夫した点。
-        </Text>
-      </WorkCard>
-      <WorkCard
-        id="tapple"
-        title="Tapple風チャットアプリ"
-        img="/tapple.png"
-        link="none"
-        skill="Next.js, Nest.js, TypeScript, MySQL, Prisma"
-        role="Webフロントエンド"
-        description="非公開です。"
-      >
-        <Text>
-          株式会社サイバーエージェントのインターンにて制作(約2週間、4人)。Webフロントを担当し、MUI, TailwindCSSを用いたUIや自動生成のOpenAPI,
-          SWRを使用してのデータフェッチを実装。
-        </Text>
-      </WorkCard>
-      <WorkCard
-        id="replace"
-        title="Re:place"
-        img="/replace.png"
-        skill="Next.js, Rails, PostgreSQL, GCP, Firebase"
-        link="replace"
-        role="Webフロント開発(UI中心)"
-        description="デモ動画(7/1リリース予定)"
-      >
-        <Text>
-          学生ITスタートアップの株式会社DOKOSOREにて制作した、エンジニア向けのオンラインイベント開催プラットフォーム。主催者も参加者もワクワクできるような機能を多数搭載。本サービスの強みである「流れるようなUI/UX」の作製を主に担当した。
-        </Text>
-      </WorkCard>
-      <WorkCard
         id="nosight"
         title="農Sightダッシュボード"
         img="/nosight.png"
-        skill="Next.js, TypeScript, React, Firebase"
+        skill="Next.js, TypeScript, Firebase"
         link="nosight"
         role="フロント開発"
         description="デモ動画"
@@ -98,6 +22,30 @@ export const WorkCardList = () => {
           </Link>
         </Text>
       </WorkCard>
+      <WorkCard
+        title="株式会社FUTUREWOODS サービスページ"
+        img="/futuresearch.png"
+        link="https://www.future-search.jp/"
+        skill="Next.js, TypeScript, TailWindCSS"
+        role="フロント開発 (UIの実装)"
+        description=""
+      >
+        <Text>
+          株式会社FUTUREWOODSの長期インターンにて、学生3人で共同開発したサービスLP。初の業務だったためGithubの使い方や命名法、コンポーネント粒度など先輩のコードを見て真似した。レスポンシブ対応に注意して制作した。
+        </Text>
+      </WorkCard>
+      <WorkCard
+        id="tukete"
+        title="Tukete"
+        img="/tukete.png"
+        skill="Next.js, TypeScript, Kotlin, Python"
+        link="tukete"
+        role="フロント開発"
+        description="製作中です。。。"
+      >
+        <Text>Sansan株式会社のインターンにて制作。</Text>
+      </WorkCard>
+
       <WorkCard
         id="terada"
         title="Reborn to 寺田蘭世"
@@ -131,6 +79,70 @@ export const WorkCardList = () => {
           GoogleForm自動入力bot。研究室の出校フォーム入力が面倒なので制作した。インターンでスクレイピングに使用していたPuppeteerを用いており、日付・時間を自動入力してくれる。
         </Text>
       </WorkCard>
+      {/* <WorkCard
+        title="株式会社FUTUREWOODS 企業ページ"
+        img="/futurewoods.png"
+        link="https://www.futurewoods.co.jp/"
+        skill="Next.js, TypeScript, TailWindCSS"
+        role="フロント開発 (UIの実装)"
+        description=""
+      >
+        <Text>
+          株式会社FUTUREWOODSの長期インターンにて、学生3人で共同開発した企業HP。Webデザイナーさんが用意した概形のデザインをうまくcssを用いてコード化した。動きがうるさすぎず、かつ飽きないといった程よいアニメーションとモダンなデザインが特徴。
+        </Text>
+      </WorkCard> */}
+      {/* <WorkCard
+        title="株式会社FUTUREWOODS 業務用ダッシュボード"
+        img="/fs-admin.png"
+        link="fs-admin"
+        skill="Nest.js, Next.js, MySQL"
+        role="フロント開発、バックエンド開発(追加機能の実装、コードの整形)"
+        description="デモ動画"
+      >
+        <Text>
+          株式会社FUTUREWOODSの長期インターンにて、先輩から引き継ぎ、運用している業務用ダッシュボード。フロントをMUIを用いたデザインにリニューアルした後、バックエンドをexpressからNest.jsに移行・整形した。通知機能や新たなソート等の機能を適宜追加している。
+        </Text>
+      </WorkCard> */}
+      {/* <WorkCard
+        title="Cell Festival"
+        img="/cellfestival.png"
+        link="https://cell-festival.vercel.app/"
+        skill="Next.js, TypeScript, React, Firebase"
+        role="フロント開発、記事の校閲、追加機能の考案"
+        description="制作中です。。"
+      >
+        <Text>
+          生物系の学習プラットフォーム制作を目指す学生団体Cell
+          Festivalのお手伝いにて、ホームページを制作。自分が生物専攻なのもあり、記事を実際に読んだり、追加機能を考えたりしながら開発している。記事の投稿、校閲をできる機能を作ったのが工夫した点。
+        </Text>
+      </WorkCard> */}
+      {/* <WorkCard
+        id="tapple"
+        title="Tapple風チャットアプリ"
+        img="/tapple.png"
+        link="none"
+        skill="Next.js, Nest.js, TypeScript, MySQL, Prisma"
+        role="Webフロントエンド"
+        description="非公開です。"
+      >
+        <Text>
+          株式会社サイバーエージェントのインターンにて制作(約2週間、4人)。Webフロントを担当し、MUI, TailwindCSSを用いたUIや自動生成のOpenAPI,
+          SWRを使用してのデータフェッチを実装。
+        </Text>
+      </WorkCard> */}
+      {/* <WorkCard
+        id="replace"
+        title="Re:place"
+        img="/replace.png"
+        skill="Next.js, Rails, PostgreSQL, GCP, Firebase"
+        link="replace"
+        role="Webフロント開発(UI中心)"
+        description="デモ動画(7/1リリース予定)"
+      >
+        <Text>
+          学生ITスタートアップの株式会社DOKOSOREにて制作した、エンジニア向けのオンラインイベント開催プラットフォーム。主催者も参加者もワクワクできるような機能を多数搭載。本サービスの強みである「流れるようなUI/UX」の作製を主に担当した。
+        </Text>
+      </WorkCard> */}
     </>
   );
 };
